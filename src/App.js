997ddf44679ff72ba24627d9d7d2a19ea6ff9c4a
ravivarma <ravivarma162000@gmail.com>
2023-09-15 08:51:19 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
+import Home from './components/Home'
 import './App.css'
-import RockPaperScissors from './components/RockPaperScissors'
 
-const choicesList = [
-  {
-    id: 'ROCK',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
-  },
-  {
-    id: 'SCISSORS',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
-  },
-  {
-    id: 'PAPER',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
-  },
-]
-
-const App = () => <RockPaperScissors choicesList={choicesList} />
+const App = () => <Home />
 
 export default App
